10dc1ce061eec2568b33aa3e4b920a742f56fc87
lox <leopold.rombaut@payby.com>
2023-01-12 09:47:02 +0100

    Initial commit

---
+++ error_handling/errors4.rs
@@ -0,0 +1,34 @@
+// errors4.rs
+// Execute `rustlings hint errors4` or use the `hint` watch subcommand for a hint.
+
+#[derive(PartialEq, Debug)]
+struct PositiveNonzeroInteger(u64);
+
+#[derive(PartialEq, Debug)]
+enum CreationError {
+    Negative,
+    Zero,
+    NaN,
+}
+
+impl PositiveNonzeroInteger {
+    fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
+        // Hmm...? Why is this only returning an Ok value?
+
+        match value {
+            x if value < 0 => Err(CreationError::Negative),
+            x if value ==0 => Err(CreationError::Zero),
+            x  => Ok(PositiveNonzeroInteger(value as u64)),
+        }
+    }
+}
+
+#[test]
+fn test_creation() {
+    assert!(PositiveNonzeroInteger::new(10).is_ok());
+    assert_eq!(
+        Err(CreationError::Negative),
+        PositiveNonzeroInteger::new(-10)
+    );
+    assert_eq!(Err(CreationError::Zero), PositiveNonzeroInteger::new(0));
+}
---
+++ options/options1.rs
@@ -0,0 +1,60 @@
+// options1.rs
+// Execute `rustlings hint options1` or use the `hint` watch subcommand for a hint.
+
+
+// This function returns how much icecream there is left in the fridge.
+// If it's before 10PM, there's 5 pieces left. At 10PM, someone eats them
+// all, so there'll be no more left :(
+fn maybe_icecream(time_of_day: u16) -> Option<u16> {
+    // We use the 24-hour system here, so 10PM is a value of 22 and 12AM is a value of 0
+    // The Option output should gracefully handle cases where time_of_day > 23.
+    // TODO: Complete the function body - remember to return an Option!
+    if time_of_day>=22
+    {
+        if time_of_day<24
+        {
+            return Some(0);
+        }
+
+        return None;
+
+    }
+
+    if time_of_day<0
+    {
+        return None;
+    }
+
+    Some(5)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_icecream() {
+        assert_eq!(maybe_icecream(9), Some(5));
+        assert_eq!(maybe_icecream(10), Some(5));
+        assert_eq!(maybe_icecream(23), Some(0));
+        assert_eq!(maybe_icecream(22), Some(0));
+        assert_eq!(maybe_icecream(25), None);
+    }
+
+    #[test]
+    fn raw_value() {
+        // TODO: Fix this test. How do you get at the value contained in the Option?
+        let icecreams : Option<u16> = maybe_icecream(25);
+
+        //unwrap can panic so try to use match instead
+        match icecreams {
+            None => println!("Days aren't that long"),
+            Some(0) => println!("No icecream :("),
+            Some(5) =>  assert_eq!(icecreams.unwrap(), 5),
+            x => panic!("unexpected invalid time"),
+        }
+
+        let icecreams : Option<u16> = maybe_icecream(20);
+        assert_eq!(icecreams, Some(5));
+    }
+}
